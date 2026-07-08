ee44a538e3a9ecb2cfd32edb564d93841448290b
lakshyarana9981-rgb <lakshyarana9981@gmail.com>
2026-07-08 22:12:55 +0530

    My first project

---
+++ Lakshya.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main () {
+    int a;
+    cout <<"enter a: "<<endl;
+    cin >> a;
+
+    int b;
+    cout <<"enter b: "<<endl;
+    cin >> b;
+
+    cout << a + b;
+    
+    return 0;
+}
